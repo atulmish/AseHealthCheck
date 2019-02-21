c76ec46ab0c9e3c745a3a3f1c7261feba2222787
DESKTOP-TGFQBC8\Atul <atul.mishra@ticketmaster.co.uk>
2019-02-21 01:26:10 +0000

    Initial

---
+++ src/AseHealthCheck/AseHealthCheck.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AseHealthCheck
+{
+    public class AseHealthCheck : IHealthCheck
+    {
+        public AseHealthCheck()
+        {
+            // Use dependency injection (DI) to supply any required services to the
+            // health check.
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Execute health check logic here. This example sets a dummy
+            // variable to true.
+            var healthCheckResultHealthy = true;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("The check indicates a healthy result."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("The check indicates an unhealthy result."));
+        }
+    }
+}
